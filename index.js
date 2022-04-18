4c5006bfab655e351a48097b55c62d697815c0b1
binamin-hussein100 <binamin.h.hassan14@gmail.com>
2022-04-18 11:31:30 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code here!
+const theMain = document.getElementById("main")
+
+theMain.remove()
+
+// adding an element
+const newHeader=document.createElement("h1")
+newHeader.setAttribute("id","victory")
+document.body.append(newHeader)
+
+// updating elements
+const myHeader = document.getElementById("victory")
+const myName = 'Binamin'
+myHeader.textContent = myName + " is the champion"
+
